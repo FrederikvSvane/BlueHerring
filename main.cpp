6e017a5cef76c168cac44737edb8a5ef61990969
Sherab Losel Matos <sherab-losel.matos@linotte.polytechnique.fr>
2024-11-28 11:23:17 +0100

    input parsing

--- main.cpp
+++ main.cpp
@@ -1,7 +1,39 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+vector<string> read_moves(string input_file) {
+    fstream file_stream;
+    file_stream.open(input_file);
+
+    string line;
+    vector<string> out;
+
+    while (getline(file_stream, line)) {
+        out.push_back(line);
+    }
+
+    return out;
+}
+
 
 int main(int argc, char const *argv[])
 {
-    std::cout << "Let's go play some chess" << std::endl;
+    if (argc != 5) {
+        printf("Wrong input size!, %i", argc);
+        return -1;
+    }
+
+    string input_file = argv[2];
+    string output_file = argv[4];
+
+    vector<string> moves = read_moves(input_file);
+
+    for (auto& move : moves) {
+        cout << move << endl;
+    }
+
     return 0;
 }
