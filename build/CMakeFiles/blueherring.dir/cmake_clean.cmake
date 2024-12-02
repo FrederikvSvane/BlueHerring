fa207745a1ba66a17b4ec175a740d1825b313826
estragoon <simon.fm.charbonneau@gmail.com>
2024-12-02 14:42:24 +0100

    adding cmake functionality

---
+++ build/CMakeFiles/blueherring.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blueherring.dir/main.cpp.o"
+  "blueherring"
+  "blueherring.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/blueherring.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
