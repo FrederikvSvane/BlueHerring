2b3cc3ecca26bbbc183ed0ba50bced1c456bf192
FrederikvSvane <s224766@student.dtu.dk>
2024-12-08 21:25:17 +0100

    Added c++ 20 requirement to cmake file

---
+++ build/CMakeFiles/blueherring.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/blueherring.dir/main.cpp.o"
+  "CMakeFiles/blueherring.dir/main.cpp.o.d"
+  "blueherring"
+  "blueherring.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/blueherring.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
