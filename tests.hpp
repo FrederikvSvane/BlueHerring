379b1345e7e9f470bb118cb94df06426dd6be30c
FrederikvSvane <s224766@student.dtu.dk>
2024-12-10 00:29:12 +0100

    Added a node per second counter to the perft suite

--- tests.hpp
+++ tests.hpp
@@ -13,7 +13,6 @@ struct perft_test_case {
     bool single_color_scenario;
 };
 
-
 uint64_t perft(board_t& board, int depth, Color color, bool single_color_only) {
     if (depth == 0)
         return 1;
@@ -31,22 +30,25 @@ uint64_t perft(board_t& board, int depth, Color color, bool single_color_only) {
     return nodes;
 }
 
-
 void run_test_suite() {
-    auto start_time = std::chrono::high_resolution_clock::now();
+    auto start_time         = std::chrono::high_resolution_clock::now();
+    uint64_t total_node_sum = 0;
 
     vector<perft_test_case> test_cases = {
         // These are taken from https://www.chessprogramming.org/Perft_Results
-        // {"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", 4, {1, 20, 400, 8902, 197281}, false},
-        // {"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R", 3, {1, 48, 2039, 97862}, false},
-        // {"8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8", 4, {1, 14, 191, 2812, 43238}, false},
-        // {"r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1", 4, {1, 6, 264, 9467, 422333}, false},
-        // {"rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R", 4, {1, 44, 1486, 62379, 2103487}, false},
-        // {"r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1", 4, {1, 46, 2079, 89890, 3894594}, false},
+        {"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", 4, {1, 20, 400, 8902, 197281}, false},
+        {"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R", 3, {1, 48, 2039, 97862}, false},
+        {"8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8", 4, {1, 14, 191, 2812, 43238}, false},
+        {"r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1", 4, {1, 6, 264, 9467, 422333}, false},
+        {"rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R", 4, {1, 44, 1486, 62379, 2103487}, false},
+        {"r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1", 4, {1, 46, 2079, 89890, 3894594}, false},
+        // These ones from http://www.rocechess.ch/perft.html
+        {"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R", 4, {1, 48, 2039, 97862, 4085603}, false},
+        {"n1n5/PPPk4/8/8/8/8/4Kppp/5N1N", 5, {1, 24, 496, 9483, 182838, 3605103}, false},
         // And these are custom made
-        {"8/P/8/8/8/8/8/8", 2, {1, 4, 44}, true}, // promotion
+        {"8/P/8/8/8/8/8/8", 2, {1, 4, 44}, true},    // promotion
         {"8/8/8/8/3p4/8/4P3/8", 2, {1, 2, 4}, false} // en passant
-        
+
     };
 
     for (const auto& test : test_cases) {
@@ -60,7 +62,8 @@ void run_test_suite() {
         bool test_passed = true;
         for (int depth = 0; depth <= test.max_depth; depth++) {
             uint64_t result = perft(board, depth, Color::WHITE, test.single_color_scenario);
-            bool passed     = result == test.expected_nodes[depth];
+            total_node_sum += result;
+            bool passed = result == test.expected_nodes[depth];
             test_passed &= passed;
 
             cout << "Depth " << depth << ": Result: " << result
@@ -74,6 +77,13 @@ void run_test_suite() {
     auto end_time = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::seconds>(end_time - start_time);
     cout << "Test suite completed in " << duration.count() << " seconds" << endl;
+
+    if (duration.count() > 0) {
+        double nps = static_cast<double>(total_node_sum) / duration.count();
+        cout << "Nodes per second (NPS): " << nps << endl;
+    } else {
+        cout << "Duration too short to calculate NPS." << endl;
+    }
 }
 
 } // namespace tests
