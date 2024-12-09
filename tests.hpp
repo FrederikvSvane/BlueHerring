5cc1fd9776cc40f343642dda745c875d6e28b82e
FrederikvSvane <s224766@student.dtu.dk>
2024-12-09 23:05:42 +0100

    added en passant test

--- tests.hpp
+++ tests.hpp
@@ -35,14 +35,17 @@ void run_test_suite() {
     auto start_time = std::chrono::high_resolution_clock::now();
 
     vector<perft_test_case> test_cases = {
-        {"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", 4, {1, 20, 400, 8902, 197281}, false},
-        {"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R", 3, {1, 48, 2039, 97862}, false},
-        {"8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8", 4, {1, 14, 191, 2812, 43238}, false},
-        {"r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1", 4, {1, 6, 264, 9467, 422333}, false},
-        {"rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R", 4, {1, 44, 1486, 62379, 2103487}, false},
-        {"r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1", 4, {1, 46, 2079, 89890, 3894594}, false},
-        {"8/P/8/8/8/8/8/8", 2, {1, 4, 44}, true}
-        // All taken from https://www.chessprogramming.org/Perft_Results
+        // These are taken from https://www.chessprogramming.org/Perft_Results
+        // {"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", 4, {1, 20, 400, 8902, 197281}, false},
+        // {"r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R", 3, {1, 48, 2039, 97862}, false},
+        // {"8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8", 4, {1, 14, 191, 2812, 43238}, false},
+        // {"r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1", 4, {1, 6, 264, 9467, 422333}, false},
+        // {"rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R", 4, {1, 44, 1486, 62379, 2103487}, false},
+        // {"r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1", 4, {1, 46, 2079, 89890, 3894594}, false},
+        // And these are custom made
+        {"8/P/8/8/8/8/8/8", 2, {1, 4, 44}, true}, // promotion
+        {"8/8/8/8/3p4/8/4P3/8", 2, {1, 2, 4}, false} // en passant
+        
     };
 
     for (const auto& test : test_cases) {
