e181be441df126fd142e6adef4d510f75881608c
estragoon <simon.fm.charbonneau@gmail.com>
2024-12-19 12:29:28 +0100

    v2

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 #include "moves.hpp"
 #include "engine.hpp"
 
-int main(int argc, char const* argv[]) // ./BlueHerring -H input.txt -m output_example.txt
+int main(int argc, char const* argv[]) // ./BlueHerring -H history.csv -m move.csv
 {
     if (argc != 5) {
         printf("Wrong input size!, %i", argc);
@@ -25,34 +25,19 @@ int main(int argc, char const* argv[]) // ./BlueHerring -H input.txt -m output_e
     board_t board;
     board.initialize_starting_board();
 
-
     // std::cout << "Starting position evaluation: " << eval::evaluate_position(board) << std::endl;
 
     vector<string> string_moves = read_moves_from_input_file(&input_file_name);
     vector<move_t> moves        = translate_moves(string_moves);
 
-    // // For printing the input file line by line (testing)
-    // cout << endl;
-    // cout << "Making the following moves:" << endl;
-    // for (const string& move : string_moves) {
-    //     cout << move << endl;
-    // }
-
     for (const move_t& move : moves) {
         moves::make_move(board, move);
     }
 
+    Color color_to_move = (moves.size() % 2 == 0) ? Color::WHITE : Color::BLACK;
 
-    // board.pretty_print_board();
-
-    move_t best_move = eval::get_best_move(board, 3 , Color::BLACK);
+    move_t best_move = eval::get_best_move(board, 3, color_to_move);
     string best_move_str = encode_move(best_move);
-
-    // cout << "Moving from: (" << best_move.from_x << "," << best_move.from_y << ") to: (" << best_move.to_x << "," << best_move.to_y << ")" << endl;
-
-    // moves::make_move(board, best_move);
-
-    // board.pretty_print_board();
 
     write_move_to_output_file(&output_file_name, &best_move_str);
 
